2763deda0593910f106a56d0bfe496e9eba32374
Ryan Chen <y.f.chenroderick@gmail.com>
2022-01-11 23:03:09 +0800

    feat(services): create api services interface
    Include todos api service.

---
+++ src/services/api-request.js
@@ -0,0 +1,71 @@
+import axios from 'axios';
+// API response type contant
+export const RESPONSE_TYPE = {
+  CONNECT_CORRECT: 'CONNECT_CORRECT',
+  CONNECT_ERROR: 'CONNECT_ERROR',
+  NETWORK_ERROR: 'NETWORK_ERROR'
+};
+// create axios instance
+const apiClient = axios.create({
+  baseURL: process.env.VUE_APP_PATH
+});
+
+export const apiClientSetToken = token => {
+  apiClient.defaults.headers.common['Authorization'] = token;
+};
+
+const responseHandler = response => {
+  console.log('api-request resolve: ', response);
+  const { headers, status, data } = response;
+  return {
+    responseType: RESPONSE_TYPE.CONNECT_CORRECT,
+    status,
+    headers,
+    data
+  };
+};
+const errorHandler = error => {
+  console.log('api-request reject: ', error.response);
+  if (error.response) {
+    const { status, statusText, data } = error.response;
+    return {
+      responseType: RESPONSE_TYPE.CONNECT_ERROR,
+      status,
+      message: data.message || statusText || 'Error! 404',
+      errorType: data.errorType || 'unknown'
+    };
+  } else {
+    return {
+      responseType: RESPONSE_TYPE.NETWORK_ERROR,
+      message: error.message || 'Network Error!'
+    };
+  }
+};
+
+// this encapsulation: to use async/await
+export const apiRequest = ({ method, url, data = {}, config = {} }) => {
+  switch (method) {
+    case 'post':
+    case 'put':
+    case 'patch':
+      return apiClient[method](url, data, config)
+        .then(response => {
+          return responseHandler(response);
+        })
+        .catch(error => {
+          return errorHandler(error);
+        });
+    case 'get':
+    case 'delete':
+      return apiClient[method](url, { params: data })
+        .then(response => {
+          return responseHandler(response);
+        })
+        .catch(error => {
+          return errorHandler(error);
+        });
+    default:
+      console.log(`undefined method: ${method}`);
+      return false;
+  }
+};
---
+++ src/services/todos-service.js
@@ -0,0 +1,31 @@
+import { apiRequest } from './api-request';
+
+export default {
+  getTodos(params) {
+    return apiRequest({
+      method: 'get',
+      url: `/todos`,
+      data: params
+    });
+  },
+  createTodo(todo) {
+    return apiRequest({
+      method: 'post',
+      url: `/todos`,
+      data: todo
+    });
+  },
+  updateTodo(id, todo) {
+    return apiRequest({
+      method: 'patch',
+      url: `/todos/${id}`,
+      data: todo
+    });
+  },
+  deleteTodo(id) {
+    return apiRequest({
+      method: 'delete',
+      url: `/todos/${id}`
+    });
+  }
+};
