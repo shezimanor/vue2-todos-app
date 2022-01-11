7f3f9622421100269d99e0a0a777f812b0d1bb9a
Ryan Chen <y.f.chenroderick@gmail.com>
2022-01-11 23:05:29 +0800

    test(BV): test BootstrapVue `VBPopover`

--- src/views/UIDemoHome.vue
+++ src/views/UIDemoHome.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class=""></div>
+  <div>
+    <div class="text-center">
+      <b-button
+        v-b-popover.hover="'I am popover content!'"
+        title="Popover Title"
+        >Hover Me</b-button
+      >
+      <b-button v-b-popover.hover title="Popover 2">Hover Me</b-button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { VBPopover } from 'bootstrap-vue';
 export default {
-  name: 'UIDemoHome'
+  name: 'UIDemoHome',
+  directives: {
+    'b-popover': VBPopover
+  }
 };
 </script>
