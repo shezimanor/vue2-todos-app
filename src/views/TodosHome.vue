ce094469b2c7388d401f5ba6218aa179fac136b2
Ryan Chen <y.f.chenroderick@gmail.com>
2022-01-11 23:08:55 +0800

    feat(views/todos): create todos views

--- src/views/TodosHome.vue
+++ src/views/TodosHome.vue
@@ -1,9 +1,35 @@
 <template>
-  <div class="home">Welcome My App</div>
+  <div>
+    <b-card-group tag="ul">
+      <b-card v-for="todo in todos" :key="todo.id" :title="todo.title">
+        <b-card-text class="text-description">
+          {{ todo.content }}
+        </b-card-text>
+        <small class="text-muted">{{ todo.id }}</small>
+      </b-card>
+    </b-card-group>
+  </div>
 </template>
 
 <script>
+import { RESPONSE_TYPE } from '@/services/api-request';
+import TodosService from '@/services/todos-service';
 export default {
-  name: 'TodosHome'
+  name: 'TodosHome',
+  data() {
+    return {
+      todos: []
+    };
+  },
+  mounted() {
+    // console.log('mounted', RESPONSE_TYPE);
+    this.getTodos();
+  },
+  methods: {
+    async getTodos() {
+      const { responseType, data } = await TodosService.getTodos();
+      if (responseType === RESPONSE_TYPE.CONNECT_CORRECT) this.todos = data;
+    }
+  }
 };
 </script>
